6ec2e7cc38fa6744d0bf47f34c1cb1f8e23d6192
PierreMear <pierre.mear35@gmail.com>
2020-01-31 10:50:50 +0100

    V0 - DLC - Ajout de configuration de tests

---
+++ src/app/pokemon/pokemon-list/pokemon-list.component.spec.ts
@@ -0,0 +1,21 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PokemonListComponent } from './pokemon-list.component';
+
+describe('PokemonListComponent', () => {
+  let component: PokemonListComponent;
+  let fixture: ComponentFixture<PokemonListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PokemonListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PokemonListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
---
+++ src/app/pokemon/services/pokemon.service.spec.ts
@@ -0,0 +1,7 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PokemonService } from './pokemon.service';
+
+describe('PokemonService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+});
